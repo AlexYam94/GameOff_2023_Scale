65d64578da2205b667f24e597663154379de175e
alex.yam <yamminghin@gmail.com>
2023-11-05 23:29:35 +0800

    Add count down when both pan weight the same

--- Scripts/Arm.gd
+++ Scripts/Arm.gd
@@ -1,20 +1,18 @@
 extends RigidBody2D
 
 @export var leftPan : Node2D
-
 @export var rightPan : Node2D
-
 @export var armVisual : Node2D
-
 @export var countDownText : Label
-
 @export var countDownTime : float
 
 var initPos
+var countDownTimer : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	initPos = position
+	countDownTimer = countDownTime
 	pass # Replace with function body.
 
 
@@ -22,5 +20,19 @@ func _ready():
 func _process(delta):
 	position = initPos
 	armVisual.rotation = rightPan.position.angle_to_point(leftPan.position)
-
+	rotation = 0
+	var leftWeight : float = leftPan.totalWeight
+	var rightWeight : float = leftPan.totalWeight
+	
+	#Count down
+	if(leftWeight == rightWeight):
+		countDownTimer = max(countDownTimer-delta, 0)
+	else:
+		countDownTimer = countDownTime
+		
+	if (countDownTimer <= 0):
+		#TODO
+		#Next level
+		pass
+		
 	pass
