9c3dc5ef2e55d14bad7e1ae7d446f4b3529c7520
alex.yam <yamminghin@gmail.com>
2023-11-21 23:21:33 +0800

    Fix gear rotation

--- Scripts/Gear.gd
+++ Scripts/Gear.gd
@@ -4,24 +4,35 @@ extends Node2D
 @export var pan : Pan
 @export var rotationAmountPerFrame : float
 
-var lastFramePos : Vector2
+var lastDiff : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	lastFramePos = pan.global_position
+	lastDiff = abs(global_position.y - pan.global_position.y)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var currentFramePos = pan.global_position
-	var diff = abs(currentFramePos.y - lastFramePos.y)
-	if(currentFramePos.y < lastFramePos.y):
-		#Rotate right
-		rotation = fmod(rotation + rotationAmountPerFrame, PI) * diff * delta
-	elif(currentFramePos.y > lastFramePos.y):
-		#Rotate left
-		rotation = fmod(rotation - rotationAmountPerFrame, PI) * diff * delta
+func _physics_process(delta):
+	var diff = abs(global_position.y - pan.global_position.y)
+	print("diff: " + str(diff))
+	print("last diff: " + str(lastDiff))
+	var newRotation = rotation
+#	if(lastDiff < diff):
+#		#Rotate right
+#		newRotation = fmod(rotation + rotationAmountPerFrame, PI) * lastDiff * delta
+#	elif(lastDiff > diff):
+#		#Rotate left
+#		newRotation = fmod(rotation - rotationAmountPerFrame, PI) * lastDiff * delta
+		
+#	rotation = newRotation
 
-	lastFramePos = currentFramePos
+	if(lastDiff < diff):
+		#Rotate right
+		rotate(-diff * delta * rotationAmountPerFrame)
+	elif(lastDiff > diff):
+#		#Rotate left
+		rotate(diff * delta * rotationAmountPerFrame)
+
+	lastDiff = diff
 	pass
