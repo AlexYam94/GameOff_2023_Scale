b6140266516da54f186dbfa2e9fe058b3443a2f4
alex.yam <yamminghin@gmail.com>
2023-11-25 00:32:53 +0800

    Add tutorial 1 event trigger

---
+++ Scripts/Tutorial1 Event Trigger.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+
+@export var nextEventInterval : float
+@export var totalEvent : int = 8
+@export var light1 : Node2D
+@export var light2 : Node2D
+@export var light3 : Node2D
+@export var wall : Node2D
+@export var light4 : Node2D
+@export var light5 : Node2D
+@export var light6 : Node2D
+
+
+var nextEventCounter : float = 0
+var startEvent : bool
+var eventCount : int = 0
+
+func _process(delta):
+	if not startEvent or eventCount > totalEvent:
+		return
+	nextEventCounter = max(nextEventCounter - delta, 0)
+	if(nextEventCounter <= 0):
+		process_event()
+		nextEventCounter = nextEventInterval
+	
+func process_event():
+	match eventCount:
+		1:
+			light2.visible = true
+		2: 
+			light3.visible = true
+		3:
+			wall.queue_free()
+		4:
+			light4.visible = true
+	eventCount += 1
+	
+
+func _on_weight_1_kg_clicked(body):
+	light1.visible = true
+	nextEventCounter = nextEventInterval
+	startEvent = true
+	eventCount = 1
